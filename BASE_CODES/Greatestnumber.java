aaac46d76bb9ecc9a1418a8cbaf06966baca740b
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-01-16 19:40:45 +0530

    GreatestNumber among two

--- BASE_CODES/Greatestnumber.java
+++ BASE_CODES/Greatestnumber.java
@@ -1,3 +1,28 @@
+// //Greatest of two numbers: 
+// import java.util.*;
+// class Greatestnumber{
+//     static void GreatestofTwonum(int num1, int num2){
+//         if(num1 > num2){
+//             System.out.println(num1+ " is greatest");
+//         }else{
+//             System.out.println(num2+ " is greatest");
+//         }
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the first number: ");
+//         int num1 = sc.nextInt();
+//         System.out.println("Enter the second number: ");
+//         int num2 = sc.nextInt();
+//         GreatestofTwonum(num1,num2);
+//     }
+// }
+
+
+
+
+
 //Greatest of two numbers: 
 import java.util.*;
 class Greatestnumber{
