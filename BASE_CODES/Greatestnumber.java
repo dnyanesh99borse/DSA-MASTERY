24befbbee00386615cc30c4e6885b5132c7e66ef
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-01-23 19:19:09 +0530

    Day-05: GreatestofThreenumber

--- BASE_CODES/Greatestnumber.java
+++ BASE_CODES/Greatestnumber.java
@@ -22,24 +22,32 @@
 
 
 
-
-//Greatest of two numbers: 
+//Greatest of three numbers: 
 import java.util.*;
-class Greatestnumber{
-    static void GreatestofTwonum(int num1, int num2){
-        if(num1 > num2){
-            System.out.println(num1+ " is greatest");
-        }else{
-            System.out.println(num2+ " is greatest");
+
+class Greatestnumber {
+
+    static void GreatestofThreenum(int num1,int num2,int num3) {
+        if (num1 > num2) {
+            if(num2 > num3){
+                System.out.println(num1 + " is greatest");
+            }else{
+                 System.out.println(num2 + " is greatest");
+            }
+        } else {
+            System.out.println(num3 + " is greatest");
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the first number: ");
         int num1 = sc.nextInt();
         System.out.println("Enter the second number: ");
         int num2 = sc.nextInt();
-        GreatestofTwonum(num1,num2);
+        System.out.println("Enter the Third number: ");
+        int num3 = sc.nextInt();
+        GreatestofThreenum(num1, num2, num3);
     }
 }
