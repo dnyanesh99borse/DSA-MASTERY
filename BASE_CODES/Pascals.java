3bf4e15691fe1054a762dc42c0a6cb9b385d7a59
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-01-24 12:04:21 +0530

    Optimal Approach of: Pascl's Triangle

--- BASE_CODES/Pascals.java
+++ BASE_CODES/Pascals.java
@@ -2,34 +2,53 @@
 
 //1. General Brute force Approach
 // LeetCode 118: Pascal's Triangle
+// class Pascals {
+
+//     static void PascalsTriangle(int n) {
+
+//         int[] prevRow = null;   // to store previous row
+
+//         for (int i = 0; i < n; i++) {
+//             int[] row = new int[i + 1];
+
+//             for (int j = 0; j <= i; j++) {
+//                 if (j == 0 || j == i) {
+//                     row[j] = 1;
+//                 } else {                                   //simple logic
+//                     row[j] = prevRow[j - 1] + prevRow[j]; //jya cell var present made ahet tyacha varchya row madhun tyachya varcha ani tyachya left sidecha.
+//                 }
+//             }
+
+//             // print row
+//             for (int val : row) {
+//                 System.out.print(val + " ");
+//             }
+//             System.out.println();
+
+//             prevRow = row;   // update previous row
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         PascalsTriangle(5);
+//     }
+// }
+
+//===========OPTIMIAL APPROACH===========
 class Pascals {
 
-    static void PascalsTriangle(int n) {
+    public static void main(String[] args) {
 
-        int[] prevRow = null;   // to store previous row
+        int n = 5;
 
         for (int i = 0; i < n; i++) {
-            int[] row = new int[i + 1];
+            int val = 1;
 
             for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    row[j] = 1;
-                } else {                                   //simple logic
-                    row[j] = prevRow[j - 1] + prevRow[j]; //jya cell var present made ahet tyacha varchya row madhun tyachya varcha ani tyachya left sidecha.
-                }
-            }
-
-            // print row
-            for (int val : row) {
                 System.out.print(val + " ");
+                val = val * (i - j) / (j + 1);
             }
             System.out.println();
-
-            prevRow = row;   // update previous row
         }
-    }
-
-    public static void main(String[] args) {
-        PascalsTriangle(5);
     }
 }
