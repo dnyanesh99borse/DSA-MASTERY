e7e2b7e6295a4c6b2b7409c45b0cfd9d5ddd8a9a
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-01-28 18:46:13 +0530

    Day_o6: Prime with single number & prime with specific range

--- BASE_CODES/Prime.java
+++ BASE_CODES/Prime.java
@@ -1,31 +1,29 @@
 import java.util.*;
-class Prime {
+class PrimewithRange{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
 
-    static boolean checkPrime(int num) {
-        boolean isPrime = true;
-        if(num < 1){
-            isPrime = false;
-        }
-        for (int i = 2; i < num; i++) {        //MAIN LOGIC OF THE CODE.
-            if (num % i == 0) {
+    System.out.print("Enter a number: ");
+    int num = sc.nextInt();
+
+    boolean isPrime = true; 
+
+    if(num < 1){
+        isPrime = false;
+    }else{
+        for(int i=2; i<num/2; i++){  //num/2 means, the only the number which is half of the number could be divide the numbe else all are the larger.
+            if(num % i == 0){
                 isPrime = false;
                 break;
             }
         }
-        return isPrime;
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-
-        boolean output = checkPrime(num);
-        if (output == true) {
-            System.out.println("the number is Prime number");
-        } else {
-            System.out.println("the number is not Prime number");
-        }
+    if(isPrime){
+        System.out.println(num+" is a Prime number");
+    }else{
+        System.out.println(num+" is not a Prime number");
     }
+   }
 }
+
+
