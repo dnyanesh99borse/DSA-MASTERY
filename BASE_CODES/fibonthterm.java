199bb0544dbcf48c5f1b9eb342ae24bbad8a8173
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-01-30 19:25:12 +0530

    Day_07: NTH FIBONACCI TERM

---
+++ BASE_CODES/fibonthterm.java
@@ -0,0 +1,28 @@
+//find the Nth term of the fibonacci series
+
+import java.util.*;
+class fibonthterm{
+    static void fibonthterm(int n){
+        if(n == 1){
+            System.out.println(n);
+        }
+
+        int a = 0;
+        int b = 1;
+        for(int i=2; i<=n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println("The " + n + "th Fibonacci term is: " + a);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        fibonthterm(n);
+
+    }
+}
