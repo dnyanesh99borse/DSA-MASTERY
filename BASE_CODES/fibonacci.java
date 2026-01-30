cd3c2af5ed4c0caf226574c2d27f0863fa6aed05
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-01-30 19:05:16 +0530

    Day_07: FIBONACCI SERIES UTPO NTH TERM

---
+++ BASE_CODES/fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class fibonacci{
+    static void fibonacciSeries(int n){
+        if(n == 0 || n == 1){
+            System.out.println(n);
+        }
+
+        int a = 0;
+        int b = 1;
+        for(int i=1; i<n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+            System.out.print(c +" ");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        fibonacciSeries(n);
+
+    }
+}
