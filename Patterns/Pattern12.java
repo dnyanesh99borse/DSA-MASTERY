a82a0d87fefd4c4528a6129883c12010bb0a5be0
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-02-23 00:17:58 +0530

    TRIANGLE STAR PATTERN

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,23 @@
+//TRIANGULAR STAR PATTERN
+class Pattern12{
+    public static void main(String[] args){
+        int n = 5;
+        for(int i=0;i<=n; i++){
+            for(int j=n; j>i; j--){
+                // System.out.print("-");
+                System.out.print(" ");
+            }
+            for(int k=0; k<i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+//     * 
+//    * * 
+//   * * * 
+//  * * * * 
+// * * * * * 
