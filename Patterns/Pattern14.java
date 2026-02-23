a89f68e22218dcafde8f17e5881bab8c613c30f7
Dnyanesh <143705815+dnyanesh99borse@users.noreply.github.com>
2026-02-23 19:52:54 +0530

    MIRROR IMAGE TRIANGLE PATTERN

---
+++ Patterns/Pattern14.java
@@ -0,0 +1,42 @@
+
+class Pattern14 {
+
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            int k = i;
+            for (int j = 0; j < i; j++) {
+                // System.out.print("-");
+                System.out.print(" ");
+            }
+            for (int j = n; j > i; j--) {
+                k++;
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n - 1; i++) {
+            int m = n-i - 1;
+            for (int j = n - 1; j > i; j--) {
+                // System.out.print("-");
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(m + (j+1) + " ");
+                
+            }
+            
+            System.out.println();
+
+        }
+    }
+}
+
+
+// 1 2 3 4 
+//  2 3 4
+//   3 4
+//    4
+//   3 4
+//  2 3 4
+// 1 2 3 4
